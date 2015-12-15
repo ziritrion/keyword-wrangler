5e6e308efe7eb9ada667787f39b1eb673c6287f5
Atope Sindrogas <ziritrion@gmail.com>
2015-12-15 14:11:41 +0100

    Milestone 2 reached
    API responds with actual database content

---
+++ src/backend/dbSession.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var DBWrapper = require('node-dbi').DBWrapper;
+
+var dbWrapper = new DBWrapper('sqlite3', {'path': '/var/tmp/keyword-wrangler.test.sqlite'});
+dbWrapper.connect();
+module.exports = dbWrapper;
